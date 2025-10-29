0bec451b1db9d94d208306a5c396fa47d585f62a
SHIVANGI SHARMA <2428bcse62@uithpu.ac.in>
2025-10-29 12:24:42 +0530

    HERE IS AN EXAMPLE OF MODULE CREATION

---
+++ g.py
@@ -0,0 +1,4 @@
+from gcd import gcd
+n1 = int(input("enter number1: "))
+n2 = int(input("enter number2: "))
+print("The gcd is:",gcd(n1,n2))
---
+++ main.py
@@ -0,0 +1,5 @@
+print("hello")
+from gc import gcd
+n1 = int(input('enter:'))
+n2 = int(input("enter: "))
+print(gcd(n1,n2))
